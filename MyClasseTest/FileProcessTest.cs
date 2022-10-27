57bf8e16d6a12fda7cd35e19d4be09b1210e5040
dougreisss <douglas_rrs@hotmail.com>
2022-10-26 21:55:21 -0300

    Tests

--- MyClasseTest/FileProcessTest.cs
+++ MyClasseTest/FileProcessTest.cs
@@ -9,11 +9,46 @@ namespace MyClasseTest
     [TestClass]
     public class FileProcessTest
     {
+        #region Propriedades e constantes 
         private const string BAD_FILE_NAME = @"C:\BadFileName.bat";
+        private const string FILE_NAME = @"FileToDeploy.txt";
         private string _GoodFileName;
 
         public TestContext TestContext { get; set; }
+        #endregion
 
+        #region Test Initialize e CleanUp
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            if (TestContext.TestName == "FileNameDoesExists")
+            {
+                setGoodFileName();
+                if (!string.IsNullOrEmpty(_GoodFileName))
+                {
+                    TestContext.WriteLine($"Creating file: {_GoodFileName}");
+                    File.AppendAllText(_GoodFileName, "Some Text");
+                }
+            }
+        }
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            if (TestContext.TestName == "FileNameDoesExists")
+            {
+                if (!string.IsNullOrEmpty(_GoodFileName))
+                {
+                    TestContext.WriteLine($"Deleting file: {_GoodFileName}");
+                    File.Delete(_GoodFileName);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Metodo auxiliar
         public void setGoodFileName()
         {
             _GoodFileName = ConfigurationManager.AppSettings["GoodFileName"];
@@ -22,8 +57,105 @@ namespace MyClasseTest
                 _GoodFileName = _GoodFileName.Replace("[AppPath]", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
             }
         }
+        #endregion
+
+        #region AreEqual/AreNotEqual Tests
 
         [TestMethod]
+        [Owner("Douglas")]
+        public void AreEqualTest() 
+        {
+            string str1 = "Douglas";
+            string str2 = "Douglas";
+
+            Assert.AreEqual(str1, str2);
+
+        }
+
+        [TestMethod]
+        [Owner("Douglas")]
+        [ExpectedException(typeof(AssertFailedException))]
+        [Ignore]
+        public void AreEqualCaseSensitiveTest() 
+        {
+            string str1 = "douglas";
+            string str2 = "douglas";
+
+            Assert.AreEqual(str1, str2, false);
+        }
+
+
+        [TestMethod]
+        [Owner("Douglas")]
+        public void AreNotEqualTest()
+        {
+            string str1 = "Douglas";
+            string str2 = "Douglas2";
+
+            Assert.AreNotEqual(str1, str2);
+        }
+
+        #endregion
+
+        #region AreSame/AreNotSame Tests
+
+        [TestMethod]
+        [Owner("Douglas")]
+        public void AreSameTest()
+        {
+            FileProcess x = new FileProcess();
+            FileProcess y = x;
+
+            Assert.AreSame(x, y);
+        }
+
+        [TestMethod]
+        [Owner("Douglas Reis")]
+        public void AreNotSameTest()
+        {
+            FileProcess x = new FileProcess();
+            FileProcess y = new FileProcess();
+
+            Assert.AreNotSame(x, y);
+        }
+
+        #endregion
+
+        #region Tests
+        [TestMethod]
+        [Owner("Douglas")]
+        [DeploymentItem(FILE_NAME)]
+        public void FileNameDoesExistsUsingDeploymentItem()
+        {
+            //Tecnica AAA
+            //Arange --> Instância classes e variaveis necessarias para rodar o teste
+            FileProcess fileProcess = new FileProcess();
+            string fileName;
+            bool fromCall;
+
+            //Action --> Realiza a ação de teste
+
+            fileName = $@"{TestContext.DeploymentDirectory}\{FILE_NAME}";
+            TestContext.WriteLine($"Checking file: {FILE_NAME}");
+            fromCall = fileProcess.FileExists(FILE_NAME);
+
+            //Assert --> Verifica a ação
+            Assert.IsTrue(fromCall);
+        }
+
+        [TestMethod]
+        [Timeout(3100)]
+        [Priority(1)]
+        public void SimulateTimeout()
+        {
+            System.Threading.Thread.Sleep(3000);
+        }
+
+        [TestMethod]
+        [Description("Check to see if a file does exists.")]
+        [Owner("Douglas")]
+        [Priority(1)]
+        [TestCategory("NoException")]
         public void FileNameDoesExists()
         {
             //Tecnica AAA
@@ -32,21 +164,20 @@ namespace MyClasseTest
             bool fromCall;
 
             //Action --> Realiza a ação de teste
-            setGoodFileName();
-            TestContext.WriteLine($"Creating file: {_GoodFileName}");
-            File.AppendAllText(_GoodFileName, "Some Text");
+          
 
             TestContext.WriteLine($"Testing file: {_GoodFileName}");
             fromCall = fileProcess.FileExists(_GoodFileName);
 
-            TestContext.WriteLine($"Deleting file: {_GoodFileName}");
-            File.Delete(_GoodFileName);
-
-            //Assert --> Verifica a ação
+             //Assert --> Verifica a ação
             Assert.IsTrue(fromCall);
         }
 
         [TestMethod]
+        [Description("Check to see if a file does not exists.")]
+        [Owner("Douglas")]
+        [Priority(1)]
+        [TestCategory("NoException")]
         public void FileNameDoesNotExists()
         {
             //Tecnica AAA
@@ -63,6 +194,10 @@ namespace MyClasseTest
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
+        [Description("Check to see if a file is null or empty")]
+        [Owner("Douglas")]
+        [Priority(0)]
+        [TestCategory("Exception")]
         public void FileNameNullOrEmpty_ThrowsArgumentNullException()
         {
             //Exception Handling
@@ -72,6 +207,10 @@ namespace MyClasseTest
         }
 
         [TestMethod]
+        [Owner("Douglas")]
+        [Description("Check to see if a file is null or empty using try Catch")]
+        [Priority(0)]
+        [TestCategory("Exception")]
         public void FileNameNullOrEmpty_ThrowsArgumentNullException_UsingTryCatch()
         {
             FileProcess fileProcess = new FileProcess();
@@ -88,5 +227,6 @@ namespace MyClasseTest
 
             Assert.Fail("Fail expected");
         }
+        #endregion
     }
 }
